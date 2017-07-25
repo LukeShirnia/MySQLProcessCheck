24eeb5e54372fa03c9fb07fb7c8a034235d66b6f
LukeShirnia <lukeshirnia@btinternet.com>
2017-07-25 12:00:28 +0000

    Updated README and Script update

--- MySQL_LongQuery_Check.py
+++ MySQL_LongQuery_Check.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 ######
 ## Author: Luke Shirnia
 # Github: https://github.com/LukeShirnia/MySQLProcessCheck
@@ -20,12 +21,12 @@ def send_email_test(long_query):
 	msg['From'] = email.utils.formataddr(('LukesLinuxPython', 'from@address.com'))
 	msg['Subject'] = 'Long Running Query!'
 	body_string = '\n'.join(long_query)
-#	print body_string # Can be used to debug if queries are being "Captured" by python script
+#	print body_string # Can be used to debug if mysql queries are being sent to email function correctly
 	body = body_string
 	msg.attach(MIMEText(body, 'plain'))
-	server = smtplib.SMTP('localhost')
+	server = smtplib.SMTP('127.0.0.1') # Requires 127. and NOT localhost on some devices. It tries to connect to IPv6 locally and this may not be enabled in "mynetworks" in postfix
 	text = msg.as_string()
-	#server.set_debuglevel(True) # debug mail if required
+#	server.set_debuglevel(True) # debug mail if required
 	server.sendmail('from@address.com', ['to@address.com.com'], text)
 	server.quit()
 	subprocess.call(["/usr/bin/logger -s 'MySQLProcessCheck: Email Sent - '`date`"], shell=True) # add a log entry for the restart
@@ -36,13 +37,16 @@ def mysql_check():
 	Connect to mysql and show the process list
 	'''
 	all_queries = []
+	exclude_queries = ['Sleep', 'Binlog Dump'] # Exclude sleeping connected users and replication process
+
 	query_string_headers = ["ID", "User", "Host", "DB", "State", "Query Time", "State", "Query"] 
 	db = MySQLdb.connect(host="", user="", passwd="", db="")
 	cur = db.cursor()
 	cur.execute("show full processlist;")
 	for row in cur.fetchall() :
-		if int(row[5]) > 10 and "Sleep" not in row[4]:
+		if int(row[5]) > 10 and row[4] not in exclude_queries:
 	        	long_query = "ID: %s  User: %s Host: %s DB: %s State: %s  Query Time: %s State: %s Query: %s"  % ( row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7] )
+#			print long_query # Used to see if queries are being picked up
 			all_queries.append(long_query)
 	all_queries = filter(None, all_queries)
 	if len(all_queries) > 1:
@@ -56,3 +60,4 @@ try:
 	mysql_check()
 except Exception as error:
 	print error
+#	subprocess.call(["/usr/bin/logger -s error"], shell=True)
--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # MySQLProcessCheck
-A python script to query MySQL processlist and report on all queries running for over a certain time. These queries will then be emailed to a specified address
+Check MySQL process list with a python script.
+<br />
+This is python script to query MySQL processlist and report on all queries running over a certain defined time in seconds. These queries will then be emailed to a specified address.
+
+<br />
+
+### Overview
+Python Script ---> Connects to MySQL
+<br />
+Checks "full processlist" query times
+<br />
+IF query time > x, send email
+<br />
+
+
+### Requirements
+* Postfix configured to send mail locally
+* Requires: mysql-python package
+
+<br />
+
+### Prerequisites
+Before running the script you need to find and replace the following throughout the script:
+* to@address.com ---> Address you wish to email long queries
+* from@address.com ---> Address you are sending emails from
+* int(row[5]) > 10 ---> Change 10 to the value in seconds you wish to use as the "long queries"
+* exclude_queries = ['Sleep', 'Binlog Dump'] ---> Add all queries here you wish to exclude
+
+<br />
+
+### How to run
+The Script is designed to run as a cron job:
+<br />
+Example: Run every 15 mins
+```
+*/15 * * * * /usr/bin/python /path/to/script/MySQLProcessCheck.py
+```
